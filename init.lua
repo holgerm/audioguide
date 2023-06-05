26ab26aaa4f81d6f832743c689d5aa608055d4a1
holgerm <mail@holgermuegge.de>
2023-06-05 22:16:25 +0200

    Varibales and filename cleared and made sound looping

--- init.lua
+++ init.lua
@@ -1,19 +1,20 @@
-soundblocks_lk1 = 0
-soundblocks_lk1_onoff = 0
+soundblocks = 0
+soundblocks_onoff = 0
  
-minetest.register_node("audioguide:audioguideblocklk1", {
-    description = "Audioguide Block Lk1",
-    tiles = {"audioguide_lk1z.png"},
+minetest.register_node("audioguide:audioblock", {
+    description = "Audioguide Block",
+    tiles = {"audioblock.png"},
     groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 2},
         on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-            if soundblocks_lk1_onoff == 0 then
-                soundblocks_lk1_onoff = 1
-                soundblocks_lk1 = minetest.sound_play("audioguide_lk1zacharias", { --name of sound, file name extension is .ogg
+            if soundblocks_onoff == 0 then
+                soundblocks_onoff = 1
+                soundblocks = minetest.sound_play("audio", { --name of sound, file name extension is .ogg
                     pos = pos, --pos where sound comes from
                     gain = 1.0,
-                    max_hear_distance = 32}) --sound gets lower the farer you get away from the jukebox
+                    max_hear_distance = 32,
+                    loop = true,}) --sound gets lower the farer you get away from the jukebox
             else
-                minetest.sound_stop(soundblocks_lk1)
-                soundblocks_lk1_onoff=0
+                minetest.sound_stop(soundblocks)
+                soundblocks_onoff=0
             end
 end})
