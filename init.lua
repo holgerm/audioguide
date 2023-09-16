0adea7b9d162a47ad18e3673fe7c7b6ab72416d5
holgerm <mail@holgermuegge.de>
2023-09-16 10:19:24 +0200

    First working draft

--- init.lua
+++ init.lua
@@ -1,37 +1,79 @@
-audionode = {}
+local audionode = {}
 
-soundblocks = 0
-soundblocks_onoff = 0
+local soundblocks = 0
+local soundblocks_onoff = 0
 
-function audionode.get_fileslist() 
+function table.tostring(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. table.tostring(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+ end
+ 
+
+function audionode.get_filestable() 
     local files = minetest.get_dir_list(minetest.get_modpath("audionode") .. '/sounds', false)
     local filestable = {}
-    local first_file = true
     for i = 1, #files do
         if files[i]:sub(-4) == ".ogg" then
-            if not first_file then
-                table.insert(filestable, ",")
-            end
             table.insert(filestable, files[i])
-            first_file = false
         end
     end
 
-    return table.concat(filestable, "")
+    return filestable
 end
 
 
 function audionode.get_formspec()
+    local filesListText = table.concat(audionode.get_filestable(), ",")
     local formspec = {
         "formspec_version[4]",
         "size[8,6]",
         "label[0.55,0.5;", minetest.formspec_escape("Pick an audiofile for this node:"), "]",
-        "textlist[0.5,0.75;7,4;mydir_list;", audionode.get_fileslist(), ";0;false]",
-        "button_exit[0.5,5;2,0.5;adioCanceled;Cancel]",
-        "button_exit[6.5,5;1,0.5;audioSelected;Ok]"
+        "textlist[0.5,0.75;7,4;audionodeFilesList;", filesListText, ";0;false]",
+        "button_exit[0.5,5;1.5,0.5;audioCanceled;Cancel]",
+        "button_exit[3.375,5;1.5,0.5;audioStop;Stop]",
+        "button_exit[6,5;1.5,0.5;audioSelected;Set]"
     }
 
     return table.concat(formspec, "")
+end
+
+audionode.state = {}
+
+function audionode.pos2key(pos)
+    return pos.x .. "-" .. pos.y .. "-" .. pos.z
+end
+
+-- we store for each audionode:
+--      with the position key (x-y-z):
+--          audiofile: the filename of the .ogg file
+--          on: 1 running, 0 stopped
+--          sound: the result of the sound_play() call which can be used to stop playback
+
+function audionode.playSound(pos)
+    local posString = tostring(pos)
+    if audionode.state[posString] then
+        if audionode.state[posString].on == 0 then
+            audionode.state[posString].on = 1
+
+            local audiofilewithoutsuffix = string.sub(audionode.state[posString].audiofile, 1, -5)
+            audionode.state[posString].sound = minetest.sound_play(audiofilewithoutsuffix, {
+                pos = pos, --pos where sound comes from
+                gain = 1.0,
+                max_hear_distance = 32,
+                loop = true,})
+        else
+            minetest.sound_stop(audionode.state[posString].sound)
+            audionode.state[posString].on = 0
+        end
+    end
 end
 
 
@@ -39,50 +81,35 @@ minetest.register_node("audionode:audio_blue", {
     description = "Audionode Block",
     tiles = {"audio_blue.png"},
     groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 2},
-    --oggEnding = ".ogg"
-    --mp3Ending = ".mp3"
     on_rightclick = function(pos, node, player, itemstack, pointed_thing)
         local formspec =  audionode.get_formspec()
-        print(formspec)
+        if not audionode.playerPos then
+            audionode.playerPos = {}
+        end
+        audionode.playerPos[tostring(player)] = pos 
 
         minetest.show_formspec(
-            player:get_player_name(), 
-            "audionode:files", 
+            player:get_player_name(),
+            "audionode:selection",
             formspec)
-
-        if soundblocks_onoff == 0 then
-            soundblocks_onoff = 1
-            soundblocks = minetest.sound_play("audio_blue", { --name of sound, file name extension is .ogg
-                pos = pos, --pos where sound comes from
-                gain = 1.0,
-                max_hear_distance = 32,
-                loop = false,}) --sound gets lower the farer you get away from the jukebox
-        else
-            minetest.sound_stop(soundblocks)
-            soundblocks_onoff=0
-        end
     end
 })
 
- 
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+    if formname ~= "audionode:selection" then
+        return
+    end
+
+    if not audionode.playerPos[tostring(player)] or not fields.audionodeFilesList then
+        return
+    end
 
 
- 
- 
--- minetest.register_node("audionode:audio_red", {
---     description = "audionode Block Red",
---     tiles = {"audio_red.png"},
---     groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 2},
---         on_rightclick = function(pos, node, player, itemstack, pointed_thing)
---             if soundblocks_onoff == 0 then
---                 soundblocks_onoff = 1
---                 soundblocks = minetest.sound_play("audio_red", { --name of sound, file name extension is .ogg
---                     pos = pos, --pos where sound comes from
---                     gain = 1.0,
---                     max_hear_distance = 32,
---                     loop = true,}) --sound gets lower the farer you get away from the jukebox
---             else
---                 minetest.sound_stop(soundblocks)
---                 soundblocks_onoff=0
---             end
--- end})
+    local pos = audionode.playerPos[tostring(player)]
+    local curState = {}
+    curState.audiofile =
+        audionode.get_filestable()[0+string.sub (fields.audionodeFilesList, 5)]
+    curState.on = 0
+    audionode.state[tostring(pos)] = curState
+    audionode.playSound(pos)
+end)
